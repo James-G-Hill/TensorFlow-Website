759a8148ec599a51a7f3a04af18545bb02fa4659
James Hill <j.g.hill@zoho.com>
2017-10-30 21:43:36 +0000

    Added code for loading in the datasets.

--- Estimator Quickstart/Tf_Estimator Quickstart.py
+++ Estimator Quickstart/Tf_Estimator Quickstart.py
@@ -17,3 +17,28 @@ IRIS_TRAINING_URL = "http://download.tensorflow.org/data/iris_training.csv"
 IRIS_TEST = "iris_test.csv"
 IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
 
+def main:
+
+    if not os.path.exists(IRIS_TRAINING):
+        raw = urllib.urlopen(IRIS_TRAINING_URL).read()
+        with open(IRIS_TRAINING, 'w') as f:
+            f.write(raw)
+
+    if not os.path.exists(IRIS_TEST):
+        raw = urllib.urlopen(IRIS_TEST_URL).read()
+        with open(IRIS_TEST, 'w') as f:
+            f.write(raw)
+
+    # Load datasets
+    # Notice that the target column & features columns are typed separately
+    training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+        filename=IRIS_TRAINING,
+        target_dtype=np.int,
+        features_dtype=np.float32)
+
+    test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+        filename=IRIS_TEST,
+        target_dtype=np.int,
+        features_dtype=np.float32)
+
+    
